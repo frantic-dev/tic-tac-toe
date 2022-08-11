013a01acc96a2fe052d615cdeb76a4ede46ca8bb
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2022-08-11 15:57:58 +0100

    alternate player moves

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const ticTacToe = (() => {
-
-    let gameBoard = ['','X','','','O','','','',''];
-
+    
+    let gameBoard = ['','','','','','','','',''];
+    
     function GameBoard (gameSpots) {
         gameBoard.push(gameSpots);
         return gameBoard;
@@ -10,21 +10,38 @@ const ticTacToe = (() => {
     let spots = document.querySelectorAll('.spot-btn');
     spots = [...spots];
 
+    function playerMoves() {
+        let movesX = gameBoard.filter((move) => move == "X").length;
+        let movesO = gameBoard.filter((move) => move == "O").length
+        if (movesX == movesO) return 'X';
+        else if (movesX > movesO) return "O"
+        }
+    
     function syncSpots() {
         for (let i = 0; i < spots.length ; i++) {
-            spots[i].addEventListener('click', () => spots[i].value = 'X')
+            spots[i].addEventListener('click', () => {
+                spots[i].value = playerMoves();
+                gameBoard[i] = spots[i].value;
+                console.log(gameBoard)
+            }
+            )}
         }
-    }
-
-    function Players (player) {
+        
+        
+    function Players (player, moves) {
         return {player};
     }
 
 
-    return {GameBoard , Players, syncSpots, spots
-    }
+    return {Players, syncSpots, spots, gameBoard}
 })();
 
+const resetBtn = document.querySelector('#reset-btn');
+
+resetBtn.addEventListener('click', () => ticTacToe.spots.forEach((spot)=> {
+    spot.value = "";
+    ticTacToe.gameBoard = ['','','','','','','','',''];
+} ))
 ticTacToe.syncSpots()
 
 
@@ -33,6 +50,4 @@ ticTacToe.syncSpots()
 
 
 
-const resetBtn = document.querySelector('#reset-btn');
 
-resetBtn.addEventListener('click', () => ticTacToe.spots.forEach((spot)=> spot.value = ""))
