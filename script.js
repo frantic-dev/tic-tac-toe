04eb138cc4e30d08fa439bb744472f5e1bd9f23c
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2022-08-11 23:33:43 +0100

    convert board array to multiD array

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const ticTacToe = (() => {
     
     let gameBoard = ['','','','','','','','',''];
     
-    function GameBoard (gameSpots) {
-        gameBoard.push(gameSpots);
-        return gameBoard;
-    }
+    // function GameBoard (gameSpots) {
+    //     gameBoard.push(gameSpots);
+    //     return gameBoard;
+    // }
 
     let spots = document.querySelectorAll('.spot-btn');
     spots = [...spots];
@@ -17,7 +17,7 @@ const ticTacToe = (() => {
         return gameBoard = ['','','','','','','','',''];
     } ))
 
-    function playerMoves() {
+    function alternateMoves() {
         let movesX = gameBoard.filter((move) => move == "X").length;
         let movesO = gameBoard.filter((move) => move == "O").length
         if (movesX == movesO) return 'X';
@@ -28,7 +28,7 @@ const ticTacToe = (() => {
         for (let i = 0; i < spots.length ; i++) {
             spots[i].addEventListener('click', () => {
                 if(spots[i].value == "") {
-                    spots[i].value = playerMoves();
+                    spots[i].value = alternateMoves();
                     gameBoard[i] = spots[i].value;
                     console.log(gameBoard)
                 }
@@ -36,7 +36,14 @@ const ticTacToe = (() => {
             )}
         }
         
-    
+        function convertToMultiD() {
+            let multiDBoard = {
+                rows : [[gameBoard[0] , gameBoard[1] , gameBoard[2]],[ gameBoard[3] , gameBoard[4] , gameBoard[5]],[gameBoard[6] , gameBoard[7] , gameBoard[8]]],
+                cols:[[gameBoard[0] ,  gameBoard[3] , gameBoard[6]],[gameBoard[1], gameBoard[4] , gameBoard[7]],[gameBoard[2] , gameBoard[5] , gameBoard[8]]],
+                across:[[gameBoard[0] , gameBoard[4] , gameBoard[8]],[gameBoard[2] , gameBoard[4] , gameBoard[6]]]
+            };
+            return multiDBoard;
+        }
 
         
     function Players (player, moves) {
@@ -44,7 +51,7 @@ const ticTacToe = (() => {
     }
 
 
-    return {Players, syncSpots, spots, gameBoard}
+    return {Players, syncSpots, spots, gameBoard, convertToMultiD}
 })();
 
 
