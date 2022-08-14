da2f87dbabab970d610ccefb6c31be350389cacd
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2022-08-14 18:44:51 +0100

    create players and shows winning player by name

--- script.js
+++ script.js
@@ -10,19 +10,19 @@ const ticTacToe = (() => {
     let spots = document.querySelectorAll('.spot-btn');
     spots = [...spots];
 
-    const resetBtn = document.querySelector('#reset-btn');
+    const restartBtn = document.querySelector('#restart-btn');
 
-    resetBtn.addEventListener('click', () => spots.forEach((spot)=> {
+    restartBtn.addEventListener('click', () => spots.forEach((spot)=> {
         spot.value = "";
         resultDiv.textContent = "";
         gameBoard = ['','','','','','','','',''];
     } ))
 
-    function alternateMoves() {
-        let movesX = gameBoard.filter((move) => move == "X").length;
-        let movesO = gameBoard.filter((move) => move == "O").length
-        if (movesX == movesO) return 'X';
-        else if (movesX > movesO) return "O"
+    function alternateMoves () {
+        let movesX = gameBoard.filter((move) => move == player1.moves).length;
+        let movesO = gameBoard.filter((move) => move == player2.moves).length
+        if (movesX == movesO) return player1.moves;
+        else if (movesX > movesO) return player2.moves
     }
     
     const resultDiv = document.querySelector('#result');
@@ -36,7 +36,7 @@ const ticTacToe = (() => {
                     console.log(gameBoard)
                     resultDiv.textContent = gameWon();
                     gameTie()
-
+                    showResult()
                 }
             }
             )}
@@ -56,17 +56,21 @@ const ticTacToe = (() => {
 
         convertToMultiD().rows.forEach((row) => {
             if (row[0] == row[1] && row[0] == row[2] && row[0] != "") {
-                return result = row[0] + " won!!!";
+                if(player1.moves === row[0]) {
+                    return result = player1.name + " won!!!";
+                } else return result = player2.name + " won!!"
             }
         })
         convertToMultiD().cols.forEach((col) => {
             if (col[0] == col[1] && col[0] == col[2] && col[0] != "") {
-                return result = col[0] + " has won biiii-";
+                if(player1.moves === col[0]) return result = player1.name+ " has won biiii-";
+                else return result = player2.name + " has won biii-";
             }
         })
         convertToMultiD().across.forEach((diagonal) => {
             if(diagonal[0] == diagonal[1] && diagonal[0] == diagonal[2] && diagonal[0] != "") {
-                return result = "oof you won " + diagonal[0];
+                if(player1.moves === diagonal[0]) return result = "oof you won " + player1.name;
+                else return result = 'oof you won ' + player2.name;
             }
         })
         return result;
@@ -79,13 +83,48 @@ const ticTacToe = (() => {
             if(emptySpots == 0 && resultDiv.textContent == "") return resultDiv.textContent = "it's a tie."
         }
     }
-        
-    function Players (player, moves) {
-        return {player};
+
+    const startBtn = document.querySelector('#start-btn');
+    const playersContainer = document.querySelector('#players-container');
+    const board = document.querySelector('#board');
+    let player1Name = document.querySelector('#player1-input');
+    let player2Name = document.querySelector('#player2-input');
+
+    startBtn.addEventListener('click', () => {
+        startBtn.setAttribute('style', 'display: none');
+       playersContainer.setAttribute('style', 'display: none;');
+        restartBtn.setAttribute('style', 'display: block');
+        board.setAttribute('style', 'display: grid');
+        getPlayers()
+    })
+
+    let player1;
+    let player2;
+
+    function getPlayers() {
+        player1Name = player1Name.value;
+        player2Name = player2Name.value;
+        player1 = Players(player1Name, "X");
+        player2 = Players(player2Name, "O")
+    }
+ 
+    const exitResultBtn = document.querySelector('#exit-result-btn');
+    const resultContainer = document.querySelector('#result-container')
+    
+    exitResultBtn.addEventListener('click', () => resultContainer.setAttribute('style', 'display: none;'))
+    
+    function showResult() {
+        if(resultDiv.textContent != "") {
+            resultContainer.style.display = "flex";
+        }
+    }
+
+    function Players (name, moves) {
+        return {name, moves};
     }
 
 
-    return {playRound}
+    return {playRound, getPlayers}
 })();
 
 
